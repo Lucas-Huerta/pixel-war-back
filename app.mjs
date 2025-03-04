47cf672ba0985f85d73651d1b01be7849f4a7fbe
Lucas Huerta <huertalucas13@gmail.com>
2025-03-04 11:42:39 +0100

    🎨 add dotenv package

--- app.mjs
+++ app.mjs
@@ -3,7 +3,9 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import { GameController } from "./src/controllers/GameController.js";
 import cors from "cors";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
